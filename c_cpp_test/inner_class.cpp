7237f7bd841fe563a1e58b18440e84a7c68da532
afschy <farhan.shahriar.2015@gmail.com>
2025-11-09 16:18:22 +0600

    removed garbage from the test folder

--- c_cpp_test/inner_class.cpp
+++ c_cpp_test/inner_class.cpp
@@ -1,23 +1,11 @@
-#include <iostream>
 struct enclose
 {
     struct inner
     {
         static int x;
-        void f(int i) {
-            std::cout << "Hello World " << i;
-        }
+        void f(int i);
     };
 };
  
 int enclose::inner::x = 1;       // definition
-// void enclose::inner::f(int i) {
-//     std::cout << "Hello World " << i;
-// } // definition
-
-int main()
-{
-    enclose::inner obj;
-    obj.f(10); // A call to the function
-    return 0;
-}
+void enclose::inner::f(int i) {} // definition
